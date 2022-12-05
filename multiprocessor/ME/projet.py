20692d2c550386b34b9160cd0d1b38064b528f66
imoussao27 <imane.moussaoui@ulb.be>
2022-12-05 21:26:24 +0100

    [DEL] repartir à zero

--- multiprocessor/ME/projet.py
+++ multiprocessor/ME/projet.py
@@ -1,14 +1,23 @@
 import Task as task
+import algorithm as algo
 
 def readFile(nameFile):
     file = open(nameFile, "r")
     textFile = file.readlines()
     tasksList = []
-
+    id = 0
     for line in textFile:
         linelist = line.split(" ")
-        newTask = task.Task(0, linelist[0], linelist[1], linelist[2], linelist[3].strip())
+        newTask = task.Task(id, int(linelist[0]), int(linelist[1]), int(linelist[2]), int(linelist[3].strip()))
         tasksList.append(newTask)
+        id += 1
 
+    return tasksList
 
-readFile("test")
+def main():
+    tasksList = readFile("test")
+    ongoingTasksList = algo.algorithm().schedulerAlgo(tasksList)
+    for element in ongoingTasksList:
+        print(element.ToString())
+
+main()
